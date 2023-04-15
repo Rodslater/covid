52bd11561d8e76fac07f537e4a1f783cff43c267
Rodslater <91638825+Rodslater@users.noreply.github.com>
2023-04-15 11:02:13 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -7,9 +7,15 @@ df <- covid19br::downloadCovid19(level = "state")
 df <- df %>% 
   select(date, code=state, epi_week, newCases, newDeaths, accumCases, accumDeaths, pop)
 
+dadosmapa <- df |>
+  filter(between(date, max(date) - 30, max(date))) %>% # Últimos 30 dias notificados
+  group_by(code) |>  
+  summarise(incidencia = round((sum(newCases) / max(pop)) * 100000, digits = 2),
+            mortalidade = round((sum(newDeaths) / max(pop)) * 100000, digits = 2),
+            letalidade = round((sum(newDeaths) / max(newCases)) * 100, digits = 2))
+
 df <- df %>% 
   filter(code == "SE")
-
 
 cidades <- covid19br::downloadCovid19("cities")
 
@@ -21,5 +27,6 @@ cidades <- cidades %>%
          mortes_100k = round(accumDeaths/pop*10^5,2)) %>% 
   select(-pop)
 
-saveRDS(df, paste0('data/df.rds'))
-saveRDS(cidades, paste0('data/cidades.rds'))
+saveRDS(df, 'data/df.rds')
+saveRDS(cidades, 'data/cidades.rds')
+saveRDS(dadosmapa, 'data/dadosmapa.rds')
